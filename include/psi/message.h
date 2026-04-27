52ecd02de30092af2f61b5a1ffe3a6e20be6c00c
Ben Siraphob <bensiraphob@gmail.com>
2026-04-26 18:50:10 -0700

    Calibrate semantic token estimates

--- include/psi/message.h
+++ include/psi/message.h
@@ -27,6 +27,13 @@ void psi_message_init_with_data(
     const char *text,
     const char *data_json
 );
+void psi_message_init_with_data_and_estimate(
+    struct psi_message *message,
+    enum psi_message_role role,
+    const char *text,
+    const char *data_json,
+    size_t token_estimate
+);
 void psi_message_free(struct psi_message *message);
 const char *psi_message_role_name(enum psi_message_role role);
 
